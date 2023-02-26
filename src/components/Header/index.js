3271b34ca0065097a0797a2778fa43f29abe7623
Ijal <faizal.ardhani12@gmail.com>
2023-02-26 22:54:56 +0700

    first commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./index.css";
+
+export default function index() {
+  return (
+    <header>
+      <h2>Made by Faizal Ardhani</h2>
+      <p>CC - 27</p>
+    </header>
+  );
+}
